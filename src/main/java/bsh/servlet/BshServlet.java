ce2814f0e9d22588dfdc0a0e6f6a45157eadd727
Ashirvad Gupta <ashirvad.gupta@broadcom.com>
2025-09-17 14:28:32 +0530

    Support Jakarta EE

--- src/main/java/bsh/servlet/BshServlet.java
+++ src/main/java/bsh/servlet/BshServlet.java
@@ -32,10 +32,10 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringReader;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import bsh.EvalError;
 import bsh.FileReader;
